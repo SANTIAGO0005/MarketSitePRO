bf9aaedc277437a76d6b35afd1b3831bb30327f9
Santiago Cifuentes <cifuentess938@gmail.com>
2022-10-31 19:21:06 -0500

    resolved errors ALLOWED

--- MarketProSite/settings/production.py
+++ MarketProSite/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://api-marketsite.herokuapp.com']
+ALLOWED_HOSTS = ['api-marketsite.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
